fc8ae01cb2c3e79a34a77dabd9bb2335ee317df5
Anoshko Alexey <AnoshkoAlexey@gmail.com>
2024-07-06 21:59:02 +0000

    Add sql query that fulfill table dog

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO DOG (ID, NAME, BREED, ORIGIN) VALUES (1, 'Fluffy', 'Pomeranian', 'Mountain View, CA');
+INSERT INTO DOG (ID, NAME, BREED, ORIGIN) VALUES (2, 'Spot', 'Pit Bull', 'Austin, TX');
+INSERT INTO DOG (ID, NAME, BREED, ORIGIN) VALUES (3, 'Ginger', 'Cocker Spaniel', 'Kansas City, KS');
+INSERT INTO DOG (ID, NAME, BREED, ORIGIN) VALUES (4, 'Lady', 'Direwolf', 'The North');
+INSERT INTO DOG (ID, NAME, BREED, ORIGIN) VALUES (5, 'Sasha', 'Husky', 'Buffalo, NY');
