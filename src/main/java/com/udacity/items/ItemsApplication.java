a239884cf089a1f6bbb84ea634271b54a92c912f
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-07-06 17:35:36 -0700

    Add settings that allow to discover items microservice

--- src/main/java/com/udacity/items/ItemsApplication.java
+++ src/main/java/com/udacity/items/ItemsApplication.java
@@ -2,8 +2,11 @@ package com.udacity.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ItemsApplication {
 
 	public static void main(String[] args) {
